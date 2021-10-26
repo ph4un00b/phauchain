10bbfb2a91d9bacd8e88830984c7671a4a30404f
phaunus <1057021+ph4un00b@users.noreply.github.com>
2021-10-25 23:28:59 -0500

    create transaction

--- dev/blockchain.ts
+++ dev/blockchain.ts
@@ -1,15 +1,52 @@
+// deno-lint-ignore-file camelcase
+
 export function create_blockchain() {
   const chain: Block[] = [];
-  let new_transactions: unknown[] = [];
+  let pending_transactions: Transaction[] = [];
 
   type Block = {
     index: number;
     timestamp: number;
-    transactions: unknown[];
+    transactions: Transaction[];
     nonce: number;
     hash: string;
     previous_block_hash: string;
   };
+
+  type Transaction = {
+    amount: number;
+    sender_address: string;
+    recipient_address: string;
+  };
+
+  function create_new_transaction({
+    amount,
+    sender_address,
+    recipient_address,
+  }: {
+    amount: number;
+    sender_address: string;
+    recipient_address: string;
+  }): number {
+    const tx: Transaction = {
+      amount,
+      sender_address,
+      recipient_address,
+    };
+
+    pending_transactions.push(tx);
+    //  console.log("push", pending_transactions);
+
+    const { index: last_transaction_index } = get_last_block();
+
+    const new_transaction_index = last_transaction_index + 1;
+
+    return new_transaction_index;
+  }
+
+  function get_last_block(): Block {
+    return chain[chain.length - 1];
+  }
 
   function create_new_block({
     nonce,
@@ -19,25 +56,29 @@ export function create_blockchain() {
     nonce: number;
     previous_block_hash: string;
     hash: string;
-  }) {
+  }): Block {
     const new_block: Block = {
       index: chain.length + 1,
       timestamp: Date.now(),
-      transactions: new_transactions,
+      transactions: pending_transactions,
       nonce,
       hash,
       previous_block_hash,
     };
 
-    new_transactions = [];
+    pending_transactions = [];
     chain.push(new_block);
 
     return new_block;
   }
 
-  return Object.freeze({
-    create_new_block,
-    chain,
-    new_transactions,
-  });
+  return function () {
+    return Object.freeze({
+      create_new_block,
+      create_new_transaction,
+      get_last_block,
+      chain,
+      pending_transactions,
+    });
+  };
 }
--- dev/blockchain_test.ts
+++ dev/blockchain_test.ts
@@ -1,23 +1,48 @@
+// deno-lint-ignore-file
 import { create_blockchain } from "./blockchain.ts";
 
 const bitcoin = create_blockchain();
 
-bitcoin.create_new_block({
+bitcoin().create_new_block({
   nonce: 1111,
   previous_block_hash: "prev_dummy_hash",
   hash: "dummy_hash",
 });
 
-bitcoin.create_new_block({
+bitcoin().create_new_transaction({
+  amount: 100,
+  sender_address: "dios_sender_address",
+  recipient_address: "juan_recipient_address",
+});
+
+bitcoin().create_new_block({
   nonce: 2222,
   previous_block_hash: "2prev_dummy_hash",
   hash: "2dummy_hash",
 });
 
-bitcoin.create_new_block({
+bitcoin().create_new_transaction({
+  amount: 50,
+  sender_address: "dios_sender_address",
+  recipient_address: "juan_recipient_address",
+});
+
+bitcoin().create_new_transaction({
+  amount: 300,
+  sender_address: "dios_sender_address",
+  recipient_address: "juan_recipient_address",
+});
+
+bitcoin().create_new_transaction({
+  amount: 2000,
+  sender_address: "dios_sender_address",
+  recipient_address: "juan_recipient_address",
+});
+
+bitcoin().create_new_block({
   nonce: 3333,
   previous_block_hash: "3prev_dummy_hash",
   hash: "3dummy_hash",
 });
 
-console.log(bitcoin);
+console.log(bitcoin());
